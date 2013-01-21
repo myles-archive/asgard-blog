594a1fba5fdae53e5691385930d716c9a882cfc7
Myles Braithwaite <me@mylesbraithwaite.com>
2013-01-21 16:36:12 -0500

    Fixing up things for Django 1.5

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 from blog.feeds import BlogPostFeed, BlogCategoryPostFeed, BlogTagPostFeed, BlogAuthorPostFeed, BlogUpdatedPostFeed
 
